ca69befce29ef54bb3395ccf2a971010f365c635
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-22 08:36:00 -0800

    create increment function that passes specs in readme

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const input = 24
+
+
+
+function increment(input) {
+  output = []
+  for (i = 0; i <= input; i++) {
+    output.push(i)
+  }
+  return output
+}
+
+
+function robert() {}
+
+
+console.log(increment(input))
