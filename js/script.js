ff9f634e1bdbd0c963b276ab16eac8e3b15a6fb8
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-22 08:49:23 -0800

    create robert function

--- js/script.js
+++ js/script.js
@@ -11,7 +11,15 @@ function increment(input) {
 }
 
 
-function robert() {}
-
+function robert(input) {
+  output = input.map(function(value) {
+    if (/3/g.test(value)) {return "Won't you be my neighbor?"}
+    else if (/2/g.test(value)) {return "Boop!"}
+    else if (/1/g.test(value)) {return "Beep!"}
+    else {return value}
+  });
+  return output
+}
 
 console.log(increment(input))
+console.log(robert(increment(input)))
